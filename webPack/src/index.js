202fe3e2be203f5d9fbec5966b72f8e1c78929be
kawether mohamed <113287643+kawether@users.noreply.github.com>
2022-12-30 16:11:40 +0200

    Add files via upload

---
+++ webPack/src/index.js
@@ -0,0 +1,31 @@
+import hello from "./hello world"
+hello()
+
+
+
+
+
+import "./style.css"
+
+
+
+import "./component/header/header"
+import"./component/cover/cover"
+
+import"./component/bio/bio"
+
+
+import"./assets/city.png"
+
+
+
+
+
+
+
+
+
+
+
+
+
